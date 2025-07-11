053df8451a9ce1d8e9f569a228634903798df1f4
gustambolopez <mogolixzq@gmail.com>
2025-07-11 18:47:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ app.use((req, res, next) => {
   if (req.url.includes("rvvASMiM")) {
     customProxy(req, res, next);
   } else {
-
-    res.status(404).send("Not Found");
+    res.status(404).send(`Cannot GET ${req.url}`);
   }
 });
 
