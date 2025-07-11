ad4a0410efe9e7910bc09b735da03d0291584eb1
gustambolopez <mogolixzq@gmail.com>
2025-07-11 18:54:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,6 @@ const PORT = 5000;
 
 app.use(cookieParser());
 
-
-app.use((req, res, next) => {
-  if (req.url.includes("rvvASMiM")) {
-    next();
-  } else {
-    next();
-  }
-});
-
 const customProxy = createProxyMiddleware({
   target: "https://html5.gamedistribution.com",
   changeOrigin: true,
@@ -27,7 +18,13 @@ const customProxy = createProxyMiddleware({
   }
 });
 
-app.use(customProxy); 
+app.use((req, res, next) => {
+  if (req.url.includes("rvvASMiM")) {
+    customProxy(req, res, next);
+  } else {
+    res.status(404).send(`Cannot GET ${req.url}`);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`FA-v2 server listening on port ${PORT}`);
