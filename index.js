2a1078495750400e6435917e18422802bd12f9e5
gustambolopez <mogolixzq@gmail.com>
2025-07-18 21:26:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,20 +13,19 @@ const Proxy = createProxyMiddleware({
   changeOrigin: true,
   ws: true,
   selfHandleResponse: false,
-  onProxyReq: (proxyReq, req, res) => {
 })
 
 // routes
 app.use((req, res, next) => {
   if (req.url.startsWith('/assets/game-imgs') || req.url.startsWith('/hosted/')) {
-    return Proxy(req, res, next) 
+    return Proxy(req, res, next)
   }
 
   // fallback
-  console.log(`Cannot get ${req.url}`)
-  res.status(404).send(`Cannot get ${req.url} `)
+  console.log(`cannot get ${req.url}`)
+  res.status(404).send(`cannot get ${req.url} `)
 })
 
 app.listen(port, () => {
-console.log(`listening on  http://localhost:${port} http://127.0.0.1:${port}`)
+  console.log(`listening on  http://localhost:${port} http://127.0.0.1:${port}`)
 })
