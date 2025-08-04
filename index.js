c2ecb18bfe1975ed0859a1ba5f0053a9af9f9706
gustambolopez <mogolixzq@gmail.com>
2025-08-04 16:37:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,32 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const { createProxyMiddleware } = require('http-proxy-middleware')
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const { createProxyMiddleware } = require("http-proxy-middleware")
 
 const app = express()
-const cookies = []
+const storedcookies = []
 const port = 8080
 
 app.use(cookieParser())
 
-const gameproxy = createProxyMiddleware({
-  target: 'https://html5.gamedistribution.com/',
+const gamedb = createProxyMiddleware({
+  target: "https://html5.gamedistribution.com/rvvASMiM/?gd_sdk_referrer_url=yjgames.gamedistribution.com",
   changeOrigin: true,
-  selfHandleResponse: true,
-
-  onProxyReq(proxyReq, req) {
-    if (cookies.length) {
-      const cookieStr = cookies.map(c => `${c.name}=${c.value}`).join('; ')
-      proxyReq.setHeader('cookie', cookieStr)
+  pathRewrite: (path, req) => {
+    if (path.includes("?")) {
+      return `${path}&gd_sdk_referrer_url=yjgames.gamedistribution.com`
+    } else {
+      return `${path}?gd_sdk_referrer_url=yjgames.gamedistribution.com`
     }
-    const url = new URL(req.originalUrl, 'http://dummy')
-    url.searchParams.set('gd_sdk_referrer_url', 'yjgames.gamedistribution.com')
-    proxyReq.path = url.pathname + url.search
   },
-
-  onProxyRes(proxyRes, req, res) {
-    // remove content-encoding header to avoid gzipped response download issue
-    delete proxyRes.headers['content-encoding']
-
-    // rewrite location header if present
-    const location = proxyRes.headers['location']
-    if (location) {
-      proxyRes.headers['location'] = location.replace(/\?gd_sdk_referrer_url=yjgames\.gamedistribution\.com/, '')
-    }
-
-    // set headers to response
-    Object.entries(proxyRes.headers).forEach(([key, value]) => {
-      res.setHeader(key, value)
+  onProxyReq: (proxyReq) => {
+    storedcookies.forEach((cookie) => {
+      proxyReq.setHeader("cookie", `${cookie.name}=${cookie.value}`)
     })
-
-    proxyRes.pipe(res)
   }
 })
 
-app.use(gameproxy)
+app.use(gamedb)
 
 app.listen(port, () => {
-  console.log(`proxy running on http://localhost:${port}`)
+  console.log(`listening on http://localhost${port}`)
 })
