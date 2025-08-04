e05c9ac84c16dc97d2046d502ad5a5e787f8a6a8
gustambolopez <mogolixzq@gmail.com>
2025-08-04 16:25:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-const express = require("express")
-const cookieParser = require("cookie-parser")
-const { createProxyMiddleware } = require("http-proxy-middleware")
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const app = express()
-const storedcookies = []
+const cookies = []
 const port = 8080
 
 app.use(cookieParser())
 
-const gamedb = createProxyMiddleware({
-  target: "https://html5.gamedistribution.com/",
+const gameproxy = createProxyMiddleware({
+  target: 'https://html5.gamedistribution.com/',
   changeOrigin: true,
-  pathRewrite: (path, req) => {
-    if (path.includes("?")) {
-      return `${path}&gd_sdk_referrer_url=yjgames.gamedistribution.com`
-    } else {
-      return `${path}?gd_sdk_referrer_url=yjgames.gamedistribution.com`
+  onProxyReq(proxyReq, req) {
+    if (cookies.length) {
+      const cookieStr = cookies.map(c => `${c.name}=${c.value}`).join('; ')
+      proxyReq.setHeader('cookie', cookieStr)
     }
-  },
-  onProxyReq: (proxyReq) => {
-    storedcookies.forEach((cookie) => {
-      proxyReq.setHeader("cookie", `${cookie.name}=${cookie.value}`)
-    })
+
+    const url = new URL(req.originalUrl, 'http://dummy')
+    url.searchParams.set('gd_sdk_referrer_url', 'yjgames.gamedistribution.com')
+
+    proxyReq.path = url.pathname + url.search
   }
 })
 
-app.use(gamedb)
+app.use(gameproxy)
 
 app.listen(port, () => {
-  console.log(`listening on http://localhost${port}`)
+  console.log(`proxy running on http://localhost:${port}`)
 })
